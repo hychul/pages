6ab7393df0285527bca8ed54112fbfd96687a7f8
hychul <hychome@gmail.com>
2021-02-28 17:05:13 +0900

    Create 'ReactPage' to save default project page

---
+++ src/page/ReactPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../static/logo.svg';
+import '../App.css';
+
+function ReactPage() {
+  return (
+    <div style={{textAlign:'center'}}>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default ReactPage;
