a76269cd50001c3b323e9f3e8ac5d13e8f8a3ce7
hychul <hychome@gmail.com>
2021-02-28 17:07:02 +0900

    Use default App css

--- src/page/ReactPage.js
+++ src/page/ReactPage.js
@@ -4,7 +4,7 @@ import 'App.css';
 
 function ReactPage() {
   return (
-    <div style={{textAlign:'center'}}>
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
