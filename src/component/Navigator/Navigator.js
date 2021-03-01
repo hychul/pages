672676ff4656afb6a5a905121f93727c15e7daf1
hychul <hychome@gmail.com>
2021-03-01 15:30:24 +0900

    Change layout display mode to flex

--- src/component/Navigator/Navigator.js
+++ src/component/Navigator/Navigator.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const nav = {
-  backgroundColor: 'red',
+  display: 'flex',
+  justifyContent: 'space-between',
   width: '100%',
   position: 'sticky',
   height: '3.5rem',
   top: '-3.5rem',
   marginTop: '0px',
+  backgroundColor: 'red'
 }
 
 function Navigator() {
@@ -14,64 +16,62 @@ function Navigator() {
     <div style={nav}>
       {/* Left */}
       <div style={{
-          display: 'inline-block',
-          height: '100%',
-          backgroundColor: 'yellow',
-          textAlign: 'center',
-          verticalAlign: 'middle',
+        display: 'flex',
       }}>
         <div style={{
-          display: 'inline-block', 
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
           width: '3.5rem', 
-          height: '100%',
-          backgroundColor: 'orange'
+          backgroundColor: 'orange',
+        }}
+        onMouseOver={() => {
+          console.log("over");
+        }}
+        onMouseOut={() => {
+          console.log("out");
+        }}
+        onClick={() => {
+          console.log("click");
         }}>
-          <div style={{
-            marginTop: 'calc(3.5rem / 2 - 0.5em)'
-          }}>
-            a
-          </div>
+          A
         </div>
         <div style={{
-          display: 'inline-block', 
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
           width: '3.5rem', 
-          height: '100%',
           backgroundColor: 'blue',
         }}>
-          <div style={{
-            marginTop: 'calc(3.5rem / 2 - 0.5em)'
-          }}>
-            LOGO
-          </div>
+          LOGO
         </div>
         <div style={{
-          display: 'inline-block',
+          // center
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          // 
           height: '100%',
-          backgroundColor: 'green'}}
-        >
-          <div style={{
-            marginTop: 'calc(3.5rem / 2 - 0.5em)',
-            marginLeft: '0.25em',
-            marginRight: '0.25em',
-            fontWeight: '650',
-            color: 'white'
-          }}>
-            Hychul's Page
-          </div>
+          backgroundColor: 'green',
+          marginLeft: '0.25rem',
+          marginRight: '0.25rem',
+          // font
+          color: 'white',
+          fontSize: '1.2em',
+          fontWeight: '700'
+        }}>
+          Hychul's Page
         </div>
       </div>
       {/* Right */}
       <div style={{
-        display: 'inline-block',
-        float: 'right',
-        width: '3.5rem',
-        height: '100%',
-        backgroundColor: 'yellow',
-        textAlign: 'center',
-        verticalAlign: 'middle'
+        display: 'flex',
       }}>
         <div style={{
-            marginTop: 'calc(3.5rem / 2 - 0.5em)'
+          width: '3.5rem',
+          backgroundColor: 'yellow',
+          textAlign: 'center',
+          verticalAlign: 'middle'
         }}>
           Right
         </div>
