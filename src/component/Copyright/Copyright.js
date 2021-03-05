d8b72b44eed1777bd7941e47d8d1a7b781b26bc3
hychul <hychome@gmail.com>
2021-03-06 01:27:46 +0900

    Refactor div-style

--- src/component/Copyright/Copyright.js
+++ src/component/Copyright/Copyright.js
@@ -3,17 +3,15 @@ import React, { Fragment } from 'react';
 function Copyright() {
   return (
     <Fragment>
-      <div 
-        style={{
-          borderTop: 'solid 1px black',
-          marginTop: '15px',
-          marginLeft: '2vmin',
-          marginRight: '2vmin',
-          paddingTop:'30px',
-          paddingBottom:'30px',
-          textAlign:'center'
-        }}
-      >
+      <div style={{
+        borderTop: 'solid 1px black',
+        marginTop: '15px',
+        marginLeft: '2vmin',
+        marginRight: '2vmin',
+        paddingTop:'30px',
+        paddingBottom:'30px',
+        textAlign:'center'
+      }}>
         © 2021. Hychul all rights reserved.
       </div>
     </Fragment>
