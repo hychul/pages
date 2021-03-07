827a1a4044acd0eedb497eef62f23f449f63270c
hychul <hychome@gmail.com>
2021-03-08 00:52:25 +0900

    Add error handling when post file is not exist

--- src/page/PostPage.js
+++ src/page/PostPage.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 
 function PostPage({match}) {
   const [post, setPost] = useState();
-  const filename = `${match.params.id}.md`;
-  const data = require(`static/post/${filename}`);
 
-  fetch(data.default).then(it => it.text()).then(it => setPost(it));
+  const filename = `${match.params.id}.md`;
+
+  try {
+    const data = require(`static/post/${filename}`);
+    fetch(data.default).then(it => it.text()).then(it => setPost(it));
+  } catch (e) {
+    setPost("The file you are looking for does not exist.");
+  }
 
   return (
     <div>
