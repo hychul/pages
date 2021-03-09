ca513623d346fe807c7173ce1d96db8680978435
hychul <hychome@gmail.com>
2021-03-09 16:00:09 +0900

    Remove not-using import

--- src/page/PostPage.js
+++ src/page/PostPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Markdown from 'component/Markdown/Markdown';
-import style from 'react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark';
 
 function PostPage({match}) {
   const filename = `${match.params.id}.md`;
