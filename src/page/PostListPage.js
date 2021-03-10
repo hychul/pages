3029d8ecae3902d35a1063cf7e54927b411ecaf4
hychul <hychome@gmail.com>
2021-03-10 15:48:52 +0900

    Add paging states

--- src/page/PostListPage.js
+++ src/page/PostListPage.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function PostListPage() {
+  const pagingSize = 2;
+  const [pageList, setPageList] = useState();
+  const [pagingNum, setPagingNum] = useState(1);
+
+  useEffect(() => {
+    const data = require(`static/post/post.meta`);
+    fetch(data.default).then(it => it.text()).then(it => {
+      let pageList = it.split('\n');
+
+      pageList = pageList.map((it) => <div key={it}>{it}</div>);
+
+      // let ret = getPagingList(pageList, 1, pagingSize);
+
+      setPageList(pageList);
+    });
+  }, []);
+
   return (
-    <div>
-      POST LIST
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: '1fr',
+      rowGap: '15px',
+      justifyContent: 'center',
+      width: '100%',
+      padding: '32px'
+    }}>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}>
+        POST LIST
+      </div>
+      <div style={{
+        display: 'grid',
+        justifyContent: 'center',
+      }}>
+        {pageList}
+      </div>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}>
+        {pagingNum} 2 3 4 5 6 7 8
+      </div>
     </div>
   )
+}
+
+function getPagingList(pageList, pagingNum, pagingSize) {
+  let ret = [];
+
+  for (let i = (pagingNum - 1) * pagingSize; i < pagingNum * pagingSize && i < pageList.length; i++) {
+    ret.push(pageList[i]);
+  }
+
+  return ret;
 }
 
 export default PostListPage;
