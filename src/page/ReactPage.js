0ef969cae7b4796973a3fa28cc1b01648e1f7cd2
hychul <hychome@gmail.com>
2021-03-10 10:28:47 +0900

    Replace some css with inline style

--- src/page/ReactPage.js
+++ src/page/ReactPage.js
@@ -4,14 +4,34 @@ import 'App.css';
 
 function ReactPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div style={{
+      width:'100%',
+      textAlign:'center',
+    }}>
+      <header 
+        style={{
+          minHeight:'100vh',
+          display:'flex',
+          flexDirection:'column',
+          alignItems:'center',
+          justifyContent:'center',
+          backgroundColor: '#282C34',
+          fontSize: 'calc(10px + 2vmin)',
+          color:'white',
+        }}
+      >
+        <img 
+          src={logo} 
+          className="App-logo"
+          alt="logo" 
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          style={{
+            color:'#61DAFB'
+          }}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
