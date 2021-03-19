a8043d60dde9809777dd0d56ccce626bc2d9976b
hychul <hychome@gmail.com>
2021-03-20 01:28:23 +0900

    Set title and date about post

--- src/page/PostPage.js
+++ src/page/PostPage.js
@@ -22,14 +22,26 @@ function PostPage({match, location}) {
       width: 'calc(100% - 33px * 2)',
       padding: '33px',
     }}>
-      <div 
-        className="Panel focus"
-        style={{
-          fontWeight: '700',
-          fontSize: '23px'
-        }}
-      >
-        {location.post.title}
+      <div className="Panel">
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          rowGap: '10px',
+          width: '100%',
+          maxWidth: '1010px',
+        }}>
+          <div style={{
+            fontWeight: '700',
+            fontSize: '23px',
+          }}>
+            {location.post.title}
+          </div>
+          <div style={{
+            color: '#6A737D',
+          }}>
+            {location.post.date}
+          </div>
+        </div>
       </div>
       <div className="Panel">
         <Markdown source={source}/>
