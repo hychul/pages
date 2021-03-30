47de13cca13809df7d68d389134bb4bb3e00a4e0
hychul <hychome@gmail.com>
2021-03-30 23:52:54 +0900

    Refactoring

--- src/redux/reducer/postList.js
+++ src/redux/reducer/postList.js
@@ -14,25 +14,27 @@ export const loadPostList = () => (dispatch, getState) => {
 
   const data = require(`static/post/post.meta`);
 
-  fetch(data.default).then(it => it.text()).then(it => {
-    const list = [];
-    it.split('\n')
-      .filter(it => !it.startsWith('//'))
-      .map(it => it.split(' :: '))
-      .filter(it => it.length >= 3)
-      .map(it => ({
-        filename: it[0],
-        date: it[1],
-        title: it[2],
-        tags: Array.from(new Set(it[3]?.split(', ').filter(it => it != "")))
-      }))
-      .forEach(it => list.push(it));
+  fetch(data.default)
+    .then(it => it.text())
+    .then(it => {
+      const list = [];
+      it.split('\n')
+        .filter(it => !it.startsWith('//'))
+        .map(it => it.split(' :: '))
+        .filter(it => it.length >= 3)
+        .map(it => ({
+          filename: it[0],
+          date: it[1],
+          title: it[2],
+          tags: Array.from(new Set(it[3]?.split(', ').filter(it => it != "")))
+        }))
+        .forEach(it => list.push(it));
 
-    dispatch({
-      type: LOAD_POST_LIST,
-      list: list
+      dispatch({
+        type: LOAD_POST_LIST,
+        list: list
+      });
     });
-  });
 }
 
 function postListReducer(state = initialState, action) {
