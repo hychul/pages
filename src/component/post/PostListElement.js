ac19b0c0560fd62f9f5b212c103c1d75043543cf
hychul <hychome@gmail.com>
2021-05-23 20:20:31 +0900

    Fix Link tag's DOM nesting issue

--- src/component/post/PostListElement.js
+++ src/component/post/PostListElement.js
@@ -27,13 +27,11 @@ function PostListElement(props) {
   ));
 
   return (
-    <Link 
-      key={filename}
-      to={{pathname: `/posts/${filename}`}} 
+    <div 
       className="Panel"
       style={{
         display: 'grid',
-        gridTemplateColumns: '2fr 1fr',
+        gridTemplateColumns: '1fr 2fr',
         gridTemplateRows: '2.5em 1.2em',
         alignItems: 'center',
         width: 'calc(100% - 1em * 2)',
@@ -41,20 +39,24 @@ function PostListElement(props) {
         margin: '0',
         padding: '0.5em 1em',
         textDecoration: 'none',
-        color: '#242A2D',
       }}
     >
-      <div style={{
-        gridColumn: '1/3',
-        gridRow: '1',
-        fontWeight: 'bold',
-        overflow: 'hidden',
-        whiteSpace: 'nowrap',
-        textOverflow: 'ellipsis',
-        fontSize: 'calc(0.75em + 1vmin)',
-      }}>
+      <Link 
+        to={{pathname: `/posts/${filename}`}} 
+        style={{
+          gridColumn: '1/3',
+          gridRow: '1',
+          textDecoration: 'none',
+          fontWeight: 'bold',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+          textOverflow: 'ellipsis',
+          fontSize: 'calc(0.75em + 1vmin)',
+          color: '#242A2D',
+        }}
+      >
         {title}
-      </div>
+      </Link>
       <div style={{
         gridColumn: '1',
         gridRow: '2',
@@ -62,7 +64,7 @@ function PostListElement(props) {
         color: '#6A737D',
       }}>
         {date}
-        </div>
+      </div>
       <div style={{
         display: 'flex',
         gridColumn: '2',
@@ -78,7 +80,7 @@ function PostListElement(props) {
         </div>
         {tags}
       </div>
-    </Link>
+    </div>
   );
 }
 
